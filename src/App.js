4d41d7d73a114db3c5f04f093112088f6a1b47cb
hendridg <hendridg@gmail.com>
2022-03-08 11:01:23 -0400

    Update components structure in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { fetchApiByDate } from './redux/home/home';
-import { BASE_URL, today } from './utils';
 import Details from './components/Details';
 import Home from './components/Home';
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchApiByDate(`${BASE_URL}${today()}`));
-  }, []);
-
   return (
     <BrowserRouter>
       <Routes>
