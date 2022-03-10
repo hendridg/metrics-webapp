596777737faf869a106a1544c22316198ff9bfd6
hendridg <hendridg@gmail.com>
2022-03-10 13:46:53 -0400

    Fix bug today() function in utils.js

--- src/utils.js
+++ src/utils.js
@@ -6,9 +6,9 @@ const today = () => {
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
   if (month < 10 && day < 10) return `${year}-0${month}-0${day}`;
-  if (month < 10 && day > 10) return `${year}-0${month}-${day}`;
-  if (month > 10 && day < 10) return `${year}-${month}-0${day}`;
-  return `/${year}-${month}-${day}`;
+  if (month < 10 && day >= 10) return `${year}-0${month}-${day}`;
+  if (month >= 10 && day < 10) return `${year}-${month}-0${day}`;
+  return `${year}-${month}-${day}`;
 };
 
 export { BASE_URL, today };
